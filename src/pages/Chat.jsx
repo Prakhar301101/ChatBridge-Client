75c3f93a3a75402a8c2b92c2b410d50fdf39f4f4
Prakhar Pandey <92363409+Prakhar301101@users.noreply.github.com>
2024-07-18 23:05:06 +0530

    Chat Page setup

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Chat = () => {
   return (
-    <div>Chat</div>
+    <div className='min-h-screen flex flex-col'>  
+      <Header/>
+      <div>chat</div>
+      <Footer/>
+    </div>
   )
 }
 
